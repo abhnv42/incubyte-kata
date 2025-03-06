dbba12a65ba1089ab6b69c5ea4120c98484686e9
Abhinav <abhnvashgpta@gmail.com>
2025-03-06 12:42:00 +0530

    handled custom delimeters

--- kata.py
+++ kata.py
@@ -5,7 +5,15 @@ def add(numbers):
     if len(numbers) == 1: return int(numbers)
     
     delimiter = ',|\\n'
+    
+    custom_delimiter_match = re.search(r'(?<=\/\/).', numbers)
+    
+    if custom_delimiter_match:
+        custom_delimeter = custom_delimiter_match.group()
+        delimiter += '|{}'.format(custom_delimeter)
+
+        numbers = re.sub(r"^//{}\n".format(custom_delimeter), '', numbers)
 
     values = {int(x) for x in re.split(delimiter, numbers)}
     
-    return sum(values);
+    return sum(values)
