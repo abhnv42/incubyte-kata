c036c1badce13c61bc0f54391c4fe9ad80ea68d6
Abhinav <abhnvashgpta@gmail.com>
2025-03-06 11:06:22 +0530

    added tests for new lines

--- test_kata.py
+++ test_kata.py
@@ -15,5 +15,11 @@ class TestKataMethods(unittest.TestCase):
     def test_multiple_numbers(self):
         self.assertEqual(add("1,5"), 6)
 
+    def test_new_lines(self):
+        self.assertEqual(add("1\n2,3"), 6)
+
+    def test_many_numbers_with_new_lines(self):
+        self.assertEqual(add("1\n2,3\n4,5"), 15)
+
 if __name__ == '__main__':
     unittest.main()
