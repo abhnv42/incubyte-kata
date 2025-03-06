73231254225aed61506d98b5536bc0e135c54a4e
Abhinav <abhnvashgpta@gmail.com>
2025-03-06 13:21:19 +0530

    handled negative values, refactored add function

--- kata.py
+++ kata.py
@@ -1,19 +1,35 @@
 import re
 def add(numbers):
-    if(type(numbers).__name__ != 'str'): return "Invalid input"
+
+    INVALID_INPUT_ERROR = "Invalid input"
+    NEGATIVE_VALUE_ERROR = "negative numbers not allowed : [{}]"
+    CUSTOM_DELIMETER_FINDER_REGEX = r'(?<=\/\/).'
+    CUSTOM_DELIMETER_DECLARATION_SUB_REGEX = r"^//{}\n"
+
+    if(type(numbers).__name__ != 'str'): raise ValueError(INVALID_INPUT_ERROR)
     if len(numbers) == 0: return 0
-    if len(numbers) == 1: return int(numbers)
+
+    if len(numbers) == 1:
+        if int(numbers) < 0: raise ValueError(NEGATIVE_VALUE_ERROR.format(numbers))
+        return int(numbers)
     
-    delimiter = ',|\\n'
-    
-    custom_delimiter_match = re.search(r'(?<=\/\/).', numbers)
+    default_delimeter = ',|\\n'
+    custom_delimiter_match = re.search(CUSTOM_DELIMETER_FINDER_REGEX, numbers)
     
     if custom_delimiter_match:
         custom_delimeter = custom_delimiter_match.group()
-        delimiter += '|{}'.format(custom_delimeter)
+        default_delimeter += '|{}'.format(custom_delimeter)
 
-        numbers = re.sub(r"^//{}\n".format(custom_delimeter), '', numbers)
+        numbers = re.sub(CUSTOM_DELIMETER_DECLARATION_SUB_REGEX.format(custom_delimeter), '', numbers)
 
-    values = {int(x) for x in re.split(delimiter, numbers)}
-    
-    return sum(values)
+    positive_numbers = []
+    negative_numbers = []
+    for x in re.split(default_delimeter, numbers):
+        if int(x) < 0: 
+            negative_numbers.append(x)
+        else:
+            positive_numbers.append(int(x))
+
+    if len(negative_numbers) > 0: raise ValueError(NEGATIVE_VALUE_ERROR.format(" ".join(negative_numbers)))
+
+    return sum(positive_numbers)
