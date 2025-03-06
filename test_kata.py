dda3ccec769537ba022ff2a4600b770ad7c26e22
Abhinav <abhnvashgpta@gmail.com>
2025-03-06 09:55:08 +0530

    added tests to check base case and arg type

--- test_kata.py
+++ test_kata.py
@@ -2,9 +2,18 @@ import unittest
 from kata import add
 
 class TestKataMethods(unittest.TestCase):
+
+    def test_wrong_arg_type(self):
+        self.assertEqual(add(1), "Invalid input")
+
     def test_empty_string(self):
-        # Assuming some_method should return True for input 1
         self.assertEqual(add(""), 0)
+
+    def test_one_number(self):
+        self.assertEqual(add("1"), 1)
+    
+    def test_multiple_numbers(self):
+        self.assertEqual(add("1, 5"), 6)
 
 if __name__ == '__main__':
     unittest.main()
